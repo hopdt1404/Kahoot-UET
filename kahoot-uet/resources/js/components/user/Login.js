cf22d6c22685dfbc44a4f863ef0af3282767988e
tkt18 <thienkhac8@gmail.com>
2020-12-01 01:01:31 +0700

    TOANG

--- kahoot-uet/resources/js/components/user/Login.js
+++ kahoot-uet/resources/js/components/user/Login.js
@@ -83,6 +83,7 @@ class Login extends Component {
   
         else if(res.data.access_token){
           localStorage.setItem('token', res.data.access_token);
+          axios.defaults.headers.common['Authorization']=`Bearer ${res.data.access_token}`;
           this.setState({
             loggedIn: true
           })
